f013890da0245b15c47d021191eeccd951bd0314
dabaitu <tom.ssf@gmail.com>
2015-04-09 21:50:44 -0700

    aliases

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cp profile ~/.profile
 cp bash_profile  ~/.bash_profile
 cp bash_prompt ~/.bash_prompt
+cp aliases ~/.aliases
 cp gitattributes ~/.gitattributes
 cp gitconfig ~/.gitconfig
 cp gitignore ~/.gitignore
